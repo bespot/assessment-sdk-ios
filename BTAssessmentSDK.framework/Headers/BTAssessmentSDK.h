53efbeced0a7fa5a9e29affa6190bef33c00f27c
spvagelis <spirou.vagelis@gmail.com>
2021-11-18 17:48:45 +0200

    - Added the BTAssessmentSDK.framework folder - Add the configure(applicationId:, applicationSecret:) method in application's AppDelegate application(_:didFinishLaunchingWithOptions:) method - Add the BTConfigurationDelegate to check the credentials - Add didConfigure(success:) and didFailConfigure(error:) methods for the BTConfigurationDelegate protocol - Update the Readme file

---
+++ BTAssessmentSDK.framework/Headers/BTAssessmentSDK.h
@@ -0,0 +1,18 @@
+//
+//  BTAssessmentSDK.h
+//  BTAssessmentSDK
+//
+//  Created by Vangelis Spirou Bespot on 30/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BTAssessmentSDK.
+FOUNDATION_EXPORT double BTAssessmentSDKVersionNumber;
+
+//! Project version string for BTAssessmentSDK.
+FOUNDATION_EXPORT const unsigned char BTAssessmentSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTAssessmentSDK/PublicHeader.h>
+
+
